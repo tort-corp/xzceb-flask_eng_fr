afcc9d6488f6c1d5ee2d029c771c0b7b14e1a7e2
tort-corp <clinton.merritt24@gmail.com>
2023-06-28 14:59:53 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,35 @@
+import unittest
+from machinetranslation.translator import english_to_french, french_to_english
+
+class TestTranslator(unittest.TestCase):
+    """Test cases for translating"""
+    def test1(self):
+        self.assertEqua
+        
+        r
+        asdf
+        
+        l(english_to_french("hello"), "bonjour")
+
+    def test2(self):
+        self.assertEqual(french_to_english("bonjour"), "hello")
+
+    def test3(self):
+        self.assertEqual(english_to_french("sing"), "chanter" )
+
+    def test4(self):
+        self.assertEqual(french_to_english("chanter"), "sing")
+
+
+if __name__ == '__main__':    
+  unittest.main(exit=False)
+
+
+
+
+
+
+
+
+
+
